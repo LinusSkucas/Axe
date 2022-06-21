f4ca8c19b9a8da39ec21cfaf064b632b2752f97e
Linus Skucas <git@linus.sh>
2022-06-21 10:03:06 -0700

    Initial Commit

---
+++ Axe/AxeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AxeApp.swift
+//  Axe
+//
+//  Created by Linus Skucas on 6/21/22.
+//
+
+import SwiftUI
+
+@main
+struct AxeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
